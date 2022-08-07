584aa869b4e5fbcd0e98d6617684c615b16d5750
Filip Huhta <filip.huhta@consid.se>
2022-08-07 17:58:53 +0200

    updated card

--- src/components/Card.js
+++ src/components/Card.js
@@ -46,16 +46,12 @@ const styles = StyleSheet.create({
 })
 
 export default function Card(props) {
-
     const getCurrentDate = (dueDate) => {
-
         var date = new Date(dueDate).getDate();
         var month = new Date(dueDate).getMonth() + 1;
         var year = new Date(dueDate).getFullYear();
 
-        //Alert.alert(date + '-' + month + '-' + year);
-        // You can turn it in to your desired format
-        return year + '-' + month + '-' + date;//format: dd-mm-yyyy;
+        return year + '-' + month + '-' + date;
     }
 
     const date = getCurrentDate(props.data.date);
@@ -67,14 +63,14 @@ export default function Card(props) {
             color={COLORS.accent}
         />
         <View style={styles.textWrapper}>
-        <Text style={{ ...styles.text, textDecorationLine: props.data.isSelected ? "line-through" : "none" }}>{props.data.text}</Text>
-        <Pressable onPress={() => props.deleteItem(props.index)} style={styles.delete}>
-            <Ionicons name="md-trash" size={24} color={COLORS.accent} />
-        </Pressable>
-        <Text style={{ ...styles.date, textDecorationLine: props.data.isSelected ? "line-through" : "none" }}>{date}</Text>
-        <Text style={{ ...styles.repeatText, textDecorationLine: props.data.isSelected ? "line-through" : "none" }}>{props.data.daily ? "(Repeats daily)" : ""}</Text>
+            <Text style={{ ...styles.text, textDecorationLine: props.data.isSelected ? "line-through" : "none" }}>{props.data.text}</Text>
+            <Pressable onPress={() => props.deleteItem(props.index)} style={styles.delete}>
+                <Ionicons name="md-trash" size={24} color={COLORS.accent} />
+            </Pressable>
+            <Text style={{ ...styles.date, textDecorationLine: props.data.isSelected ? "line-through" : "none" }}>{date}</Text>
+            <Text style={{ ...styles.repeatText, textDecorationLine: props.data.isSelected ? "line-through" : "none" }}>{props.data.daily ? "(Repeats daily)" : ""}</Text>
 
         </View>
-      
+
     </Pressable>
 }
