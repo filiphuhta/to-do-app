8d40c5ffce7d7a5663aa0f2be627c6a0fa64d8bf
Filip Huhta <filip.huhta@consid.se>
2022-10-02 14:45:09 +0200

    made it possible to add items to list

--- src/screens/ToDoList/ToDoList.js
+++ src/screens/ToDoList/ToDoList.js
@@ -8,7 +8,8 @@ import {
    TextInput,
    TouchableOpacity,
    FlatList,
-   Alert
+   Alert,
+   Pressable
 } from "react-native";
 import {
    COLORS,
@@ -18,6 +19,7 @@ import {
 } from "../../constants";
 import { setItem, setObjectItem, getObjectItem } from '../../utils/storage';
 import { ListItem } from '../../components/ListItem';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 const styles = StyleSheet.create({
@@ -38,21 +40,43 @@ const styles = StyleSheet.create({
       backgroundColor: COLORS.bgSecondary,
       borderWidth: 2,
       maxWidth: 500,
+   },
+   topWrapper: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      marginTop: 20,
+      borderBottomColor: COLORS.secondary,
+      borderBottomWidth: 2,
+      maxWidth: 500,
+   },
+   addButton: {
+      borderWidth: 1,
+      borderColor: 'rgba(0,0,0,0.2)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 50,
+      height: 50,
+      backgroundColor: COLORS.bgSecondary,
+      borderRadius: 100,
+      position: "absolute",
+      right: 0,
+      top: -10
    }
 })
 
 export default function ToDoList() {
    const [list, setList] = useState([]);
-   //State variables
-   //let storedTasks;
-/** 
+
    useEffect(() => {
-      getObjectItem("to-do-list")
-         .then(t => setList(t.item))
-            .catch(e => { console.log(e) });
-      //  updateTasks();
-   }), [];
-   */
+      getObjectItem("lists")
+         .then(t => {
+            if (t.status === "success") {
+               setList(t.item)
+            }
+         })
+         .catch(e => { console.log(e) });
+   },[]);
+
    const AsyncInput = async () => {
       return new Promise((resolve, reject) => {
          Alert.prompt("Create list",
@@ -70,12 +94,17 @@ export default function ToDoList() {
             ],
             "plain-text");
       })
-   }
+   };
 
    const addTask = async () => {
+      let data = list ? list : [];
       let userInput = await AsyncInput();
       if (userInput) {
-         setList(oldArray => [...oldArray, { name: userInput }]);
+         data.push({ name: userInput });
+         setList(data); // Adding a JS Object
+         //  setObjectItem("tasks", data);
+         //  setList(oldArray => [...oldArray, { name: userInput }]);
+         setObjectItem("lists", data);
       } else {
          alert("Please type in a name of the list")
       }
@@ -84,8 +113,8 @@ export default function ToDoList() {
    // A function that delete an item at position idx from the list array
    const deleteItem = (idx) => {
       Alert.alert(
-         "Delete task",
-         "Are you sure you want to delete this task?",
+         "Delete List",
+         "Are you sure you want to delete this list?",
          [
             {
                text: "Cancel",
@@ -95,20 +124,26 @@ export default function ToDoList() {
                text: "Yes", onPress: () => {
                   const data = list.filter((item, index) => index !== idx);
                   setList(data);
-                  setObjectItem("to-do-list", data);
+                  setObjectItem("lists", data);
                }
             }
          ])
-   }
+   };
 
    return (
       <View style={styles.container}>
-         <View style={styles.textBoxWrapper}>
-            <TouchableOpacity
-               style={styles.btn}
-               onPress={() => addTask()}>
-               <Text style={{ fontSize: 16, color: COLORS.text }}>Add task +</Text>
-            </TouchableOpacity>
+         <View style={styles.topWrapper}>
+            <Text style={{
+
+               ...FONTS.h1_semiBold,
+               color: COLORS.text,
+               marginBottom: 15,
+
+
+            }}>Your Lists</Text>
+            <Pressable style={styles.addButton} onPress={() => addTask()}>
+               <Ionicons name="md-add" size={24} color={COLORS.accent} />
+            </Pressable>
          </View>
          <FlatList style={{ flex: 1, top: 10 }}
             data={list}
